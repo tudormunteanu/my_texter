72bdec9b2123838037158d5799455c27b209a67d
Bernardo Roschke <bernardoroschke@gmail.com>
2016-08-07 02:08:35 +0000

    login

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Thinkful Capstone project. Create a texting app which individuals can enroll and select desired cadance. 
 
 This will text users reminders to drink water or other messages. 
+
+Start with readme. 
+
+then database
+
+core functionality in command line
--- texter/views.py
+++ texter/views.py
@@ -12,3 +12,20 @@ from flask.ext.login import current_user
 @app.route("/")
 def home():
     return render_template("base.html")
+    
+    
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
