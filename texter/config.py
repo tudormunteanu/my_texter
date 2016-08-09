74594fa657cb119f1f7f97243a2458302a4bd92d
Bernardo Roschke <bernardoroschke@gmail.com>
2016-08-09 20:22:17 +0000

    updates

--- texter/config.py
+++ texter/config.py
@@ -2,5 +2,6 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:thinkful@localhost:5432/texter"
     DEBUG = True
+    SECRET_KEY = os.environ.get("TEXTER_SECRET_KEY", os.urandom(12))
     
     
