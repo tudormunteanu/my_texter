4bc2d5df5e5332fa9e60a493e3235a6537190f06
tudor <tmunteanu@thinkful.com>
2016-08-21 13:37:04 +0000

    update manage py

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ from flask.ext.script import Manager
 from texter import app
 from getpass import getpass
 from werkzeug.security import generate_password_hash
-
+from flask.ext.migrate import Migrate, MigrateCommand
+from texter.database import Base
 
 manager = Manager(app)
 
@@ -11,7 +12,13 @@ manager = Manager(app)
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+    
+class DB(object):
+    def __init__(self, metadata):
+        self.metadata = metadata
 
+migrate = Migrate(app, DB(Base.metadata))
+manager.add_command('db', MigrateCommand)
 
 if __name__ == "__main__":
     manager.run()
